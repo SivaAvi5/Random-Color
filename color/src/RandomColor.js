1bf40cc11dd1b9b419c9172c5d2cad3f39f2d025
SivaAvi5 <avissiva229@gmail.com>
2024-02-22 16:08:31 +0530

    final:rgb,hex and toggle text are added

--- color/src/RandomColor.js
+++ color/src/RandomColor.js
@@ -1,65 +1,80 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const RandomColor = () => {
+    const[typeOfColor,setTypeOfColor] = useState('hex')
+    const[color,setColor] = useState('#000000')
+    const[toggleText,setToggleText] = useState('true')
 
-    const[color,setColor] = useState('#123456')
-    const [colorType,setColorType] = useState('hex')
+    const randomColorUtility = (length) => {
+        return Math.floor(Math.random()*length)
+    }
 
-    const handleHexColor = () => {
-       const colorDigits = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'] 
-        let hex = '#'
-        for(let i =0; i < 6; i++){
-            const randomCode =  Math.floor(Math.random()*colorDigits.length)
-            console.log(randomCode)
-            console.log(randomCode)
-            hex += colorDigits[randomCode]
+    const handleCreateRandomHexColor = () => {
+        const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+        let hexColor = '#'
+        for(let i=0; i<6; i++){
+            hexColor += hex[randomColorUtility(hex.length)]
         }
-        console.log(hex)
-        setColor(hex)
+        setColor(hexColor)
     }
-    
+    const handleCreateRandomRgbColor = () => {
+        const r = randomColorUtility(256)
+        const g = randomColorUtility(256)
+        const b = randomColorUtility(256)
 
-    const handleRgbColor = () =>{
-       let r = Math.floor(Math.random()*255)
-       let g = Math.floor(Math.random()*255)
-       let b = Math.floor(Math.random()*255)
-       
-       setColor(`rgb(${r},${g},${b})`)
+        setColor(`rgb(${r},${g},${b})`)
     }
-    console.log(color)
 
-    const handleColorType = (color) =>{
-        setColorType(color)
-        color === 'hex'? setColor('#000000'):setColor('rgb(20,0,0)')
+    useEffect(() => {
+        if(typeOfColor === 'rgb'){
+            handleCreateRandomRgbColor()
+        }else{
+            handleCreateRandomHexColor()
+        }
+    },[typeOfColor])
 
-    }
+    console.log(toggleText)
 
   return (
-    <div>
-        <div style={{
+    <div 
+        style={{
+            width:'100vw',
             height:'100vh',
-            width:'100vh',
-            background: color
-        }}>
-            <button onClick={() => handleColorType('hex')}>Hex Color</button>
-            <button onClick={() => handleColorType('rgb')}>RGB color</button>
-            <button style={{
-                padding:'2em',
-                margin:'20%'
-
-            }} onClick={colorType === 'hex'?() => handleHexColor():() => handleRgbColor()}>random color</button>
-            <div style={{
-                color:'whitesmoke',
-                display:'flex',
-                justifyContent:'center',
-                fontSize:'2rem'
-            }}>  
+            background: color,
+            display:'flex',
+            flexDirection:'column',
+            justifyContent:'flex-start',
+            alignItems:'center',
+        }}
+    >
+        <div
+            style={{
+                marginTop:'40px'
+            }}
+        >
+            <button onClick={() => setTypeOfColor('hex')}>Create HEX Color</button>
+            <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
+            <button onClick={typeOfColor === 'hex' ? () => handleCreateRandomHexColor(): () => handleCreateRandomRgbColor()}>Generate Random Color</button>
+            <button onClick={() => setToggleText(!toggleText)}>Toggle Text</button>
+        </div>
+        <div
+        style={{
+            marginTop:'20%',
+            display:'flex',
+            flexDirection:'column',
+            alignItems:'center',
+            justifyContent:'center',
+            color: toggleText? 'white':'black',         
+            fontSize:'2rem',  
+        }}
+        >
+            <h3>
                 {
-                colorType === 'hex'? <div><div>Hex Color</div><div>{color}</div></div>:<div><div>Rgb</div><div>{color}</div></div>
-              
+                    typeOfColor === 'rgb' ? 'RGB Color' : "HEX Color"
                 }
-            </div>
-           
+            </h3>
+            <h1>{color}</h1>
+
         </div>
     </div>
   )
